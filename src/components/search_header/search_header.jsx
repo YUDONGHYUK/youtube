71004d6c246f796cfbd561194cdb0e19d597cfaf
doyu <ydh0905@nate.com>
2022-04-11 00:02:22 +0900

    make search bar & function

---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,45 @@
+import React, { useRef } from "react";
+import styles from "./search_header.module.css";
+
+const Search_header = ({ onSearch }) => {
+  const inputRef = useRef();
+
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    onSearch(value);
+  };
+
+  const onClick = () => {
+    handleSearch();
+  };
+
+  const onKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+  return (
+    <header className={styles.header}>
+      <div className={styles.log}>
+        <img alt="logo" src="/images/logo.png" className={styles.img} />
+        <h1 className={styles.title}>Youtube</h1>
+      </div>
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type="search"
+        placeholder="Search..."
+        onKeyPress={onKeyPress}
+      />
+      <button className={styles.button} type="submit" onClick={onClick}>
+        <img
+          alt="search"
+          src="/images/search.png"
+          className={styles.buttonImg}
+        />
+      </button>
+    </header>
+  );
+};
+
+export default Search_header;
