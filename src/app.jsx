750edac7a28a383ec9659d6579d1e8baad98d16a
doyu <ydh0905@nate.com>
2022-04-15 17:03:48 +0900

    add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -3,40 +3,15 @@ import styles from "./app.module.css";
 import Search_header from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
 
   const search = (query) => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyAU3VZv56xLsyPq1U06iw59WTBYU1ryQF4`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log("error", error));
+    youtube.search(query).then(videos => setVideos(videos))
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAU3VZv56xLsyPq1U06iw59WTBYU1ryQF4",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+    youtube.mostPoPular().then(videos => setVideos(videos))
   }, []);
 
   return (
